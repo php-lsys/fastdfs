edf0553b117f133f6857078a194e020c218bea9e
shanliu <shan.liu@msn.com>
2018-09-27 09:29:49 +0800

    ..

--- src/classes/LSYS/FastDFS.php
+++ src/classes/LSYS/FastDFS.php
@@ -7,20 +7,20 @@
  */
 namespace LSYS;
 class FastDFS extends \FastDFS{
-    protected $_connect_server;
     /**
      * @throws Exception
      */
     public function connect_server_from_tracker($re_connect=false){
-        if ($this->_connect_server&&!$re_connect)return $this->_connect_server;
+        static $connect_server_bak;
+        if ($connect_server_bak&&!$re_connect)return $connect_server_bak;
         $tracker = $this->tracker_get_connection();
         if (!$tracker||!$this->active_test($tracker)){
             throw new Exception('FASTDFS[active]:'.$this->get_last_error_info(),$this->get_last_error_no());
         }
-        $this->_connect_server=$this->connect_server($tracker['ip_addr'], $tracker['port']);
-        if (!$this->_connect_server){
+        $connect_server_bak=$this->connect_server($tracker['ip_addr'], $tracker['port']);
+        if (!$connect_server_bak){
             throw new Exception('FASTDFS[connect]:'.$this->get_last_error_info(),$this->get_last_error_no());
         }
-        return $this->_connect_server;
+        return $connect_server_bak;
     }
 }
